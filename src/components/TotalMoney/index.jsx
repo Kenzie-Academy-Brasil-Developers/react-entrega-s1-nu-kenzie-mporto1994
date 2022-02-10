767c86983e540a436c21863828a55dbd466cba57
Matheus <mporto1994@gmail.com>
2022-02-10 11:39:35 -0300

    total is right, now

--- src/components/TotalMoney/index.jsx
+++ src/components/TotalMoney/index.jsx
@@ -3,7 +3,7 @@ import {useState} from "react"
 
 export const TotalMoney = ({transactions}) => {
     const total = () =>{
-        return(transactions.reduce((total,atual)=>(total+atual.value),0))
+        return(transactions.reduce((total,atual)=>(atual.type==="Saída"?total-atual.value:total+atual.value),0))
     }
 
     return(
